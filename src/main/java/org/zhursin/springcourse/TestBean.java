b8166506f54b3881ef93028d975b93368594c5f6
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2023-02-09 13:53:19 +0600

    Initial commit

---
+++ src/main/java/org/zhursin/springcourse/TestBean.java
@@ -0,0 +1,17 @@
+package org.zhursin.springcourse;
+
+public class TestBean {
+  private String name;
+
+  public TestBean(final String name) {
+    this.name = name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
